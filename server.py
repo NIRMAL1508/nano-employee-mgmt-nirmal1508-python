247871bfa42f25809e1e4dadd31d1e3ff7559dde
NIRMAL1508 <muthukumaresannirmal@gmail.com>
2023-09-06 16:26:55 +0530

    commit 6

--- server.py
+++ server.py
@@ -1,9 +1,8 @@
-from flask import Flask
-from flask import request
-from flask import jsonify
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db'  # SQLite in-memory database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db'  # SQLite database
 db = SQLAlchemy(app)
 
 # Define a database model
@@ -12,58 +11,49 @@ class Employee(db.Model):
     name = db.Column(db.String(100))
     city = db.Column(db.String(100))
 
-db.create_all() 
+db.create_all()
 
-
-app = Flask(__name__)
-employee_database = []
 def generate_employee_id():
-    return str(len(employee_database) + 1)
-# Greeting 
+    return str(Employee.query.count() + 1)
+
+# Greeting
 @app.route("/greeting", methods=['GET'])
 def greeting():
     return 'Hello world!'
-
 
 # Create Employee
 @app.route('/employee', methods=['POST'])
 def create_employee():
     data = request.get_json()
 
-    # Generate a unique employee ID (You can implement this as needed)
+    # Generate a unique employee ID
     employee_id = generate_employee_id()
 
-    employee = {
-        "employeeId": employee_id,
-        "name": data['name'],
-        "city": data['city']
-    }
+    employee = Employee(
+        employeeId=employee_id,
+        name=data['name'],
+        city=data['city']
+    )
 
-    employee_database.append(employee)
+    db.session.add(employee)
+    db.session.commit()
 
     return jsonify({"employeeId": employee_id}), 201
-
 
 # Get all Employee details
 @app.route('/employees/all', methods=['GET'])
 def get_all_employees():
-    return jsonify(employee_database)
-
-# Get all Employee details
-def find_employee_by_id(employee_id):
-    for employee in employee_database:
-        if employee['employeeId'] == employee_id:
-            return employee
-    return None
+    employees = Employee.query.all()
+    employee_list = [{"employeeId": employee.employeeId, "name": employee.name, "city": employee.city} for employee in employees]
+    return jsonify(employee_list)
 
 # Get Employee details
 @app.route('/employee/<id>', methods=['GET'])
 def get_employee(id):
-    employee = find_employee_by_id(id)
-    if employee is None:
+    employee = Employee.query.get(id)
+    if not employee:
         return jsonify({"message": f"Employee with {id} was not found"}), 404
-    return jsonify(employee)
-
+    return jsonify({"employeeId": employee.employeeId, "name": employee.name, "city": employee.city})
 
 def evaluate_filters(employee, filters, condition="AND"):
     if not filters:
@@ -82,9 +72,9 @@ def apply_filter(employee, filter_data):
     neq_value = filter_data.get("neq")
 
     if field_name == "name":
-        return (eq_value and employee["name"] == eq_value) or (neq_value and employee["name"] != neq_value)
+        return (eq_value and employee.name == eq_value) or (neq_value and employee.name != neq_value)
     elif field_name == "city":
-        return (eq_value and employee["city"] == eq_value) or (neq_value and employee["city"] != neq_value)
+        return (eq_value and employee.city == eq_value) or (neq_value and employee.city != neq_value)
     else:
         return False  # Unknown field name
 
@@ -102,9 +92,10 @@ def search_employees():
 
     condition = search_request.get("condition", "AND")
 
-    matching_employees = [employee for employee in employee_database if evaluate_filters(employee, fields, condition)]
+    employees = Employee.query.all()
+    matching_employees = [employee for employee in employees if evaluate_filters(employee, fields, condition)]
 
     return jsonify(matching_employees), 200
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
