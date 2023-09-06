24ff03e5a118891d609bb51eb7e594c1622e283a
NIRMAL1508 <muthukumaresannirmal@gmail.com>
2023-09-06 16:44:58 +0530

    commit 10

--- server.py
+++ server.py
@@ -2,9 +2,9 @@ from flask import Flask, request, jsonify
 import json
 
 app = Flask(__name__)
-employee_data_file = 'employees.json'
+employee_data_file = 'employee_data.json'  # JSON file to store employee data
 
-# Load existing employee data from the JSON file
+# Function to load employee data from the JSON file
 def load_employee_data():
     try:
         with open(employee_data_file, 'r') as file:
@@ -12,22 +12,23 @@ def load_employee_data():
     except FileNotFoundError:
         return []
 
-# Save employee data to the JSON file
+# Function to save employee data to the JSON file
 def save_employee_data(data):
     with open(employee_data_file, 'w') as file:
         json.dump(data, file, indent=4)
 
+# Greeting
 @app.route("/greeting", methods=['GET'])
 def greeting():
     return 'Hello world!'
 
+# Create Employee
 @app.route('/employee', methods=['POST'])
 def create_employee():
     data = request.get_json()
 
-    employee_data = load_employee_data()
-
     # Generate a unique employee ID based on the number of existing employees
+    employee_data = load_employee_data()
     employee_id = str(len(employee_data) + 1)
 
     employee = {
@@ -43,11 +44,13 @@ def create_employee():
 
     return jsonify({"employeeId": employee_id}), 201
 
+# Get all Employee details
 @app.route('/employees/all', methods=['GET'])
 def get_all_employees():
     employee_data = load_employee_data()
     return jsonify(employee_data)
 
+# Get Employee details by ID
 @app.route('/employee/<id>', methods=['GET'])
 def get_employee(id):
     employee_data = load_employee_data()
